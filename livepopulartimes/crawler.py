21f5ba35d5f4f073dd107f68c86a440e3fc1d859
Brian Chen <ihasdapi@gmail.com>
2020-04-26 02:28:49 -0700

    fixed edge case w/ opening_hours

--- livepopulartimes/crawler.py
+++ livepopulartimes/crawler.py
@@ -198,12 +198,17 @@ def format_and_add_param(detail, api_key, get_detail):
     """
     address = detail["formatted_address"] if "formatted_address" in detail else detail.get("vicinity", "")
     place_id = "{} {}".format(detail["name"], address)
-
+    
+    try:
+        hours = detail["opening_hours"]
+        
+    except:
+        hours = None
     detail_json = {
 
         "place_id": detail["place_id"],
         "name": detail["name"],
-        "hours": detail["opening_hours"],
+        "hours": hours,
         "place_types": detail["types"],
         "coordinates": detail["geometry"]["location"]
     }
